69048f477561af987bbb08ab931f7e8b22f33b14
Roshan <lalasarthakr@gmail.com>
2024-01-18 14:40:24 +0530

    new commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 
 import React from "react";
 import './../styles/App.css';
+import {useState} from "react"
 
 const App = () => {
+  const [name,setName]=useState("");
+  function handleInput(e){
+    setName(e.target.value);
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+       
+        <input type="text" placeholder="Enter Name" onChange={handleInput}></input>
+        <h1>{name}</h1>
+
     </div>
   )
 }
